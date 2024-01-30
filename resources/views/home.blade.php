769d6470d164d034aed8e56104752d59ef1ff6ac
guhftld <123229419+DaraH06@users.noreply.github.com>
2024-01-30 16:42:51 +0700

    home-udah

---
+++ resources/views/home.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>ED HOTEL</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav>
+        <div class="nav-logo">
+            <a href="#">
+                <img src="images/img 2.png">
+            </a>
+        </div>
+        <div class="nav-menu">
+            <ul class="nav-links">
+                <li class="link"><a href="#">HOME</a></li>
+                <li class="link"><a href="#">ABOUT US</a></li>
+                <li class="link"><a href="#">ROOMS</a></li>
+                <li class="link"><a href="#">PAGES</a></li>
+                <li class="link"><a href="#">CONTACT</a></li>
+            </ul>
+            <div class="menu">
+                <img src="images/hamberger.png" alt="hambaerger">
+            </div>
+        </div>
+    </nav>
+    <!-- #region -->
+    <header class="container">
+        <div class="content">
+            <H1>Amazing Suite <span>With River View </span>& Lot OF Services</H1>
+            <button class="btn-0">View Our Rooms</button>
+        </div>
+        <div class="image">
+            <img src="images/img 1.jpg">
+        </div>
+    </header>
+    <!-- =============================== -->
+    <div class="welcome">
+        <img src="images/img3.jpeg" alt="Hotel Lobby">
+        <div class="pendahuluan">
+            <div class="judul">
+                <h2>WELCOME TO OUR</h2>
+                <h2 style="color: rgb(255, 157, 29); ">HOTEL LOTUS</h2>
+            </div>
+            <p>LEdhotel Lotusは、ジャワ島東部のシトゥボンド、パンジに位置するホテルです。交通の便が良く、シトゥボンド駅からわずか3.17kmの距離にあります。ホテル内にはエアコン、レストラン、24時間対応のフロントデスク、駐車場、共有エリアで利用可能なWiFiなど、快適な滞在に必要な設備が整っています。
+
+                このホテルは、パンジのSMKN 1によって運営されており、清潔で快適な客室と親切なスタッフによるサービスが評価されています。また、シトゥボンドを訪れる役人の宿泊先としてもよく利用されています。
+                
+                詳細については、電話番号(0338) 4590207までお問い合わせください。素晴らしい休暇をお過ごしください</p>
+        </div>
+    </div>
+    <!-- KONTEN -->
+    <div class="overlap-group-wrapper">
+        <div class="overlap-group">
+            <div class="rectangle"></div>
+            <div class="text-wrapper">MAKANAN</div>
+            <img class="img" src="images/tajin.jpeg" />
+            <img class="img-2" src="images/tajin.jpeg" />
+            <img class="img-3" src="images/tajin.jpeg" />
+        </div>
+    </div>
+    <!-- fasilitas -->
+    <section id="facilities">
+        <div class="rectangle">
+            <div class="title">FACILITIES & SERVICES</div>
+            <div class="images">
+                <img src="images/wifi.png">
+                <img src="images/sarapan.png">
+                <img src="images/gym.png">
+            </div>
+        </div>
+        <img class="kasur" src="images/kasur.png">
+    </section>
+    <script src="script.js"></script>
+</body>
+</html>
